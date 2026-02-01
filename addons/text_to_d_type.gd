f55ae5b63abffc5f18fff3cf2effd01737213576
bazbrad765 <bazbrad@gmail.com>
2026-01-31 21:10:49 -0800

    update

--- addons/text_to_d_type.gd
+++ addons/text_to_d_type.gd
@@ -6,16 +6,6 @@ extends Control
 @onready var output = $VBoxContainer/output
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_input_text_changed():
 	if o.selected == 0:
 		var words = input.text.split(" ")
@@ -26,12 +16,10 @@ func _on_input_text_changed():
 	elif o.selected == 1:
 		var words = input.text.split(" ")
 		var pairs = []
-
 		for i in range(0, words.size(), 2):
 			if i + 1 < words.size(): 
 				var key = '"' + words[i] + '"'
 				var value = words[i + 1]
 				pairs.append(key + ": " + value)
-		
 		output.text = ", ".join(pairs)
 		
